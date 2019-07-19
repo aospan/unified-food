c33a29fa945c56b89551d1ac34b59cd33e1811f9
Abylay Ospan <aospan@netup.ru>
2019-07-19 09:37:05 -0400

    Calculate required ingredients proportions
    Python script to Calculate required ingredients proportions
    
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

---
+++ compile_food.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#
+# Unified food project
+#
+# Calculate required ingredients proportions
+# to feed human with specified profile (for example, healthy_adult.yaml)
+# 
+# Example:
+# ./compile_food.py --profile profiles/healthy_adult.yaml \
+#   --ingredient ingredients/whole_oat_flour.yaml \
+#   --ingredient ingredients/milled_flax.yaml \
+#   --output myblend.yaml
